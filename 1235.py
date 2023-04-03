a1fec11777a307ae894963879cf559c8ae128814
TheBlackBeardPirate <128419785+TheBlackBeardPirate@users.noreply.github.com>
2023-04-03 15:08:04 -0300

    Update 1235.py

--- 1235.py
+++ 1235.py
@@ -7,11 +7,11 @@ while n > 0:
     j = 0
     for i in range(int(tam / 2) - 1, -1, -1):
         nome2 = nome2 + nome[i]
-        j = j + 1
+        j += 1
 
     for i in range(tam - 1, int(tam / 2) - 1, -1):
         nome2 = nome2 + nome[i]
-        j = j + 1
+        j += 1
 
     print(nome2)
-    n = n - 1
+    n -= 1
